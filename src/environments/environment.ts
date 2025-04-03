0c646d5a617d17a3eae2454a1c5707b128e548b2
Ally Elvis Nzeyimana <allyelvis6569@gmail.com>
2025-04-03 12:35:13 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
-export const environment = {
-  firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
-  },
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCo6_oNMRkd01HMwNUfkime1SHg1DF5IaQ",
+  authDomain: "aenzbi-cloud-project.firebaseapp.com",
+  databaseURL: "https://aenzbi-cloud-project-default-rtdb.firebaseio.com",
+  projectId: "aenzbi-cloud-project",
+  storageBucket: "aenzbi-cloud-project.firebasestorage.app",
+  messagingSenderId: "371058325493",
+  appId: "1:371058325493:web:89927c8f364165edc792bc",
+  measurementId: "G-XBX05D70PH"
 };
