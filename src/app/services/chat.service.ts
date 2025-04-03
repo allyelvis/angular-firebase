ee3b0f93ec7a45c18e19f0cd7d2d76a78f0859b6
Ally Elvis Nzeyimana <allyelvis6569@gmail.com>
2025-04-03 12:55:37 +0200

    Update chat.service.ts

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -73,49 +73,71 @@ export class ChatService {
     });
   }
 
-  // Login Friendly Chat.
-  login() {}
+  // // Signs-in Friendly Chat.
+login() {
+    signInWithPopup(this.auth, this.provider).then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        this.router.navigate(['/', 'chat']);
+        return credential;
+    })
+}
 
-  // Logout of Friendly Chat.
-  logout() {}
-
-  // Adds a text or image message to Cloud Firestore.
-  addMessage = async (
-    textMessage: string | null,
-    imageUrl: string | null
-  ): Promise<void | DocumentReference<DocumentData>> => {};
-
-  // Saves a new message to Cloud Firestore.
-  saveTextMessage = async (messageText: string) => {
-    return this.addMessage(messageText, null);
-  };
-
-  // Loads chat messages history and listens for upcoming ones.
-  loadMessages = () => {
-    return null as unknown;
-  };
-
-  // Saves a new message containing an image in Firebase.
-  // This first saves the image in Firebase storage.
-  saveImageMessage = async (file: any) => {};
-
-  async updateData(path: string, data: any) {}
-
-  async deleteData(path: string) {}
-
-  getDocData(path: string) {}
-
-  getCollectionData(path: string) {}
-
-  async uploadToStorage(
-    path: string,
-    input: HTMLInputElement,
-    contentType: any
-  ) {
-    return null;
+ // Logout of Friendly Chat.
+logout() {
+    signOut(this.auth).then(() => {
+        this.router.navigate(['/', 'login'])
+        console.log('signed out');
+    }).catch((error) => {
+        console.log('sign out error: ' + error);
+    })
+}
+// observable that is updated when the auth state changes
+user$ = user(this.auth);
+ // Adds a text or image message to Cloud Firestore.
+addMessage = async (
+  textMessage: string | null,
+  imageUrl: string | null,
+): Promise<void | DocumentReference<DocumentData>> => {
+  // ignore empty messages
+  if (!textMessage && !imageUrl) {
+    console.log(
+      "addMessage was called without a message",
+      textMessage,
+      imageUrl,
+    );
+    return;
   }
-  // Requests permissions to show notifications.
-  requestNotificationsPermissions = async () => {};
 
-  saveMessagingDeviceToken = async () => {};
+  if (this.currentUser === null) {
+    console.log("addMessage requires a signed-in user");
+    return;
+  }
+
+  const message: ChatMessage = {
+    name: this.currentUser.displayName,
+    profilePicUrl: this.currentUser.photoURL,
+    timestamp: serverTimestamp(),
+    uid: this.currentUser?.uid,
+  };
+
+  textMessage && (message.text = textMessage);
+  imageUrl && (message.imageUrl = imageUrl);
+
+  try {
+    const newMessageRef = await addDoc(
+      collection(this.firestore, "messages"),
+      message,
+    );
+    return newMessageRef;
+  } catch (error) {
+    console.error("Error writing new message to Firebase Database", error);
+    return;
+  }
+};
+  // Loads chat message history and listens for upcoming ones.
+loadMessages = () => {
+  // Create the query to load the last 12 messages and listen for new ones.
+  const recentMessagesQuery = query(collection(this.firestore, 'messages'), orderBy('timestamp', 'desc'), limit(12));
+  // Start listening to the query.
+  return collectionData(recentMessagesQuery);
 }
